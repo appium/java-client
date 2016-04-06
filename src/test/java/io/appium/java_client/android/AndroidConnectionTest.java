8250fd1e0a46d7f7e5d7b94319ac60d28a76d418
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2016-04-06 21:19:28 +0300

    #325: unused import was removed

--- src/test/java/io/appium/java_client/android/AndroidConnectionTest.java
+++ src/test/java/io/appium/java_client/android/AndroidConnectionTest.java
@@ -21,7 +21,6 @@ import io.appium.java_client.NetworkConnectionSetting;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
