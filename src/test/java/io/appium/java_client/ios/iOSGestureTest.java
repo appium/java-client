5da85f818d9bfd04b8be7aa7e38ecbcccd72e16c
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2016-04-08 16:58:54 +0300

    #325 fix & #350 fix: iOS gesture test

--- src/test/java/io/appium/java_client/ios/iOSGestureTest.java
+++ src/test/java/io/appium/java_client/ios/iOSGestureTest.java
@@ -13,136 +13,41 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.appium.java_client.ios;
 
-import io.appium.java_client.*;
-import io.appium.java_client.remote.MobileCapabilityType;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import org.junit.*;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.SwipeElementDirection;
+import org.junit.Test;
 
-import java.io.File;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Test Mobile Driver features
- */
-public class iOSGestureTest {
+public class IOSGestureTest extends BaseIOSTest {
 
-    private static AppiumDriverLocalService service;
-    private AppiumDriver<WebElement> driver;
 
-    @BeforeClass public static void beforeClass() throws Exception {
-        service = AppiumDriverLocalService.buildDefaultService();
-        service.start();
+    @Test public void tapTest() {
+        ((IOSElement) driver.findElementById("IntegerA")).setValue("2");
+        ((IOSElement) driver.findElementById("IntegerB")).setValue ("4");
+
+        MobileElement e = driver.findElementByAccessibilityId("ComputeSumButton");
+        driver.tap(2, e, 2000);
+        assertEquals(driver.findElementByXPath("//*[@name = \"Answer\"]").getText(), "6");
     }
 
-    @AfterClass public static void afterClass() {
-        if (service != null)
-            service.stop();
+    @Test public void zoomTest() {
+        MobileElement e = driver.findElementById ("IntegerA");
+        driver.zoom(e);
     }
 
-    @Before public void setup() throws Exception {
-        if (service == null || !service.isRunning())
-            throw new RuntimeException("An appium server node is not started!");
-
-        File appDir = new File("src/test/java/io/appium/java_client");
-        File app = new File(appDir, "TestApp.app.zip");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.4");
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Simulator");
-        capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
-        driver = new IOSDriver<WebElement>(service.getUrl(), capabilities);
+    @Test public void pinchTest() {
+        MobileElement e = driver.findElementById ("IntegerA");
+        driver.pinch(e);
     }
 
-    @After public void tearDown() throws Exception {
-        driver.quit();
-    }
-
-    @Test public void TouchActionTest() throws InterruptedException {
-        WebElement button = driver.findElementsByClassName("UIAButton").get(3);
-        TouchAction action = new TouchAction(driver);
-        action.press(button).perform();
-        Thread.sleep(2000);
-    }
-
-    @Test public void TouchActionChainTest() throws InterruptedException {
-        WebDriverWait wait = new WebDriverWait(driver, 2);
-
-        WebElement button = driver.findElementsByClassName("UIAButton").get(5);
-        TouchAction action = new TouchAction(driver);
-        action.press(button).perform();
-
-        wait.until(ExpectedConditions.alertIsPresent());
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
-
-        WebElement mapview = driver.findElementByXPath("//UIAWindow[1]/UIAMapView[1]");
-        action = new TouchAction(driver);
-        action.press(mapview).moveTo(mapview, 0, 100).release().perform();
-        Thread.sleep(2000);
-    }
-
-    @Test public void MultiGestureTest() throws InterruptedException {
-        WebDriverWait wait = new WebDriverWait(driver, 2);
-
-        WebElement button = driver.findElementsByClassName("UIAButton").get(5);
-        TouchAction action = new TouchAction(driver);
-        action.press(button).perform();
-
-        wait.until(ExpectedConditions.alertIsPresent());
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
-
-        WebElement mapview = driver.findElementByXPath("//UIAWindow[1]/UIAMapView[1]");
-
-        MultiTouchAction multiTouch = new MultiTouchAction(driver);
-        TouchAction action0 =
-            new TouchAction(driver).press(mapview, 100, 0).moveTo(mapview, 0, -80).release();
-        TouchAction action1 =
-            new TouchAction(driver).press(mapview, 100, 50).moveTo(mapview, 0, 80).release();
-        multiTouch.add(action0).add(action1).perform();
-        Thread.sleep(2000);
-    }
-
-    @Test public void ZoomTest() throws InterruptedException {
-        WebDriverWait wait = new WebDriverWait(driver, 2);
-
-        WebElement button = driver.findElementsByClassName("UIAButton").get(5);
-        TouchAction action = new TouchAction(driver);
-        action.press(button).perform();
-
-        wait.until(ExpectedConditions.alertIsPresent());
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
-
-        WebElement mapview = driver.findElementByXPath("//UIAWindow[1]/UIAMapView[1]");
-
-        driver.zoom(mapview);
-        Thread.sleep(2000);
-    }
-
-    @Test public void TapSingleFingerTest() {
-        driver.tap(1, 100, 200, 1000);
-    }
-
-    @Test public void elementGestureTest() {
-        MobileElement e = (MobileElement) driver.findElementByName("TextField1");
-        e.tap(1, 1500);
-        e.zoom();
-        e.pinch();
-        e.swipe(SwipeElementDirection.UP, 2000);
-        e.swipe(SwipeElementDirection.UP, 5, 5, 2000);
-        e.swipe(SwipeElementDirection.DOWN, 2000);
-        e.swipe(SwipeElementDirection.DOWN, 5, 5, 2000);
-        e.swipe(SwipeElementDirection.LEFT, 2000);
-        e.swipe(SwipeElementDirection.LEFT, 5, 5, 2000);
-        e.swipe(SwipeElementDirection.RIGHT, 2000);
-        e.swipe(SwipeElementDirection.RIGHT, 5, 5, 2000);
+    @Test public void horizontalSwipingTest() {
+        MobileElement slider = driver.findElementByClassName ("UIASlider");
+        slider.swipe(SwipeElementDirection.LEFT, 2, 0, 3000);
+        assertEquals("0%", slider.getAttribute("value"));
+        slider.swipe(SwipeElementDirection.RIGHT, 2, 0, 3000);
+        assertEquals("100%", slider.getAttribute("value"));
     }
 }
