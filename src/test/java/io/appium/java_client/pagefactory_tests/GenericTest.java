e65beca10b70cbdd2f5deccd7dde69bf7a8274ba
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2016-05-01 19:38:15 +0300

    GenericTest uses mocked objects

--- src/test/java/io/appium/java_client/pagefactory_tests/GenericTest.java
+++ src/test/java/io/appium/java_client/pagefactory_tests/GenericTest.java
@@ -1,101 +1,97 @@
 package io.appium.java_client.pagefactory_tests;
 
 
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.ios.IOSDriver;
-import io.appium.java_client.pagefactory.*;
-import io.appium.java_client.pagefactory_tests.widgets.RottenTomatoes;
-import io.appium.java_client.pagefactory_tests.widgets.ios.simple.IOSMovie;
-import io.appium.java_client.remote.IOSMobileCapabilityType;
-import io.appium.java_client.remote.MobileCapabilityType;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import org.junit.*;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
-import java.io.File;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.Assert.assertTrue;
+import java.util.Set;
 
 public class GenericTest {
 
-    private static AppiumDriverLocalService service;
-    private static RottenTomatoes rottenTomatoes;
-    public IOSDriver<?> driver;
+    static class TempGenericPage<T> {
 
-    static class NewArrivalsPage<T> {
+        public List<T> items;
 
-        @iOSFindBy(className = "UIATableCell") List<IOSMovie> movieList;
-
-        public List<T> getNewMovies() {
-            return (List<T>) movieList;
+        public List<T> getItems() {
+            return items;
         }
-
     }
 
-    static class NewArrivals extends NewArrivalsPage<Object> {
+    static class MockWebDriver implements WebDriver {
 
-        @iOSFindBy(xpath = "//UIAButton[@name='New Arrivals']")
-        public MobileElement newlyArrived;
+        @Override
+        public void get(String url) {
 
+        }
+
+        @Override
+        public String getCurrentUrl() {
+            return null;
+        }
+
+        @Override
+        public String getTitle() {
+            return null;
+        }
+
+        @Override
+        public List<WebElement> findElements(By by) {
+            return null;
+        }
+
+        @Override
+        public WebElement findElement(By by) {
+            return null;
+        }
+
+        @Override
+        public String getPageSource() {
+            return null;
+        }
+
+        @Override
+        public void close() {
+
+        }
+
+        @Override
+        public void quit() {
+
+        }
+
+        @Override
+        public Set<String> getWindowHandles() {
+            return null;
+        }
+
+        @Override
+        public String getWindowHandle() {
+            return null;
+        }
+
+        @Override
+        public TargetLocator switchTo() {
+            return null;
+        }
+
+        @Override
+        public Navigation navigate() {
+            return null;
+        }
+
+        @Override
+        public Options manage() {
+            return null;
+        }
     }
 
     @Test
-    public void genericTestCase() {
-        NewArrivals newArrivals = new NewArrivals();
-        PageFactory.initElements(new AppiumFieldDecorator(driver),
-            newArrivals);
-        newArrivals.newlyArrived.click();
-        assertTrue(newArrivals.getNewMovies().size() >= 1);
-
-    }
-
-    /**
-     * initialization.
-     */
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        service = AppiumDriverLocalService.buildDefaultService();
-        service.start();
-    }
-
-    /**
-     * finishing.
-     */
-    @AfterClass
-    public static void afterClass() throws Exception {
-        if (service != null) {
-            service.stop();
-        }
-    }
-
-    /**
-     * The setting up.
-     */
-    @Before
-    public void setUp() throws Exception {
-        File appDir = new File("src/test/java/io/appium/java_client");
-        File app = new File(appDir, "RottenTomatoes.zip");
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.3");
-        //sometimes environment has performance problems
-        capabilities.setCapability(IOSMobileCapabilityType.LAUNCH_TIMEOUT, 500000);
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Simulator");
-        capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
-        driver = new IOSDriver<>(service.getUrl(), capabilities);
-    }
-
-    /**
-     * finishing.
-     */
-    @After
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+    public void genericTestCse() {
+        PageFactory.initElements(new AppiumFieldDecorator(new MockWebDriver()), new TempGenericPage<Object>());
     }
 }
