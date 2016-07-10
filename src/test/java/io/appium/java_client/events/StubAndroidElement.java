1351cf291f966397c15ff9115ec450eeb0341e05
TikhomirovSergey <tichomirovsergey@gmail.com>
2016-07-10 22:10:34 +0300

    #242 fix: Everything was finished

---
+++ src/test/java/io/appium/java_client/events/StubAndroidElement.java
@@ -0,0 +1,27 @@
+package io.appium.java_client.events;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StubAndroidElement extends AndroidElement {
+    @Override public String toString() {
+        return this.getClass().getCanonicalName();
+    }
+
+    @Override public MobileElement findElement(String by, String using) {
+        return new StubAndroidElement();
+    }
+
+    @Override public List<MobileElement> findElements(String by, String using) {
+        List<MobileElement> result = new ArrayList<>();
+        result.add(new StubAndroidElement());
+        result.add(new StubAndroidElement());
+        return result;
+    }
+
+    public void replaceValue(String value) {
+    }
+}
