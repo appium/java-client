bd0a86c7da09f22b850267f4fe49245194bb383c
sai krishna <saikrishna321@yahoo.com>
2016-11-18 12:52:59 +0530

    fixed codacy style

--- src/test/java/io/appium/java_client/android/UIAutomator2Test.java
+++ src/test/java/io/appium/java_client/android/UIAutomator2Test.java
@@ -3,6 +3,7 @@ package io.appium.java_client.android;
 import io.appium.java_client.remote.AutomationName;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServerHasNotBeenStartedLocallyException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -26,7 +27,8 @@ public class UIAutomator2Test {
         service.start();
 
         if (service == null || !service.isRunning()) {
-            throw new RuntimeException("An appium server node is not started!");
+            throw new AppiumServerHasNotBeenStartedLocallyException
+                ("An appium server node is not started!");
         }
 
         File appDir = new File("src/test/java/io/appium/java_client");
