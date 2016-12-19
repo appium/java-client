635ef9b6f6f40ba766b51aa46cd85197a28eee65
Enrique Daimiel <kdaimiel@gmail.com>
2016-12-19 17:15:20 +0100

    Organize imports

--- src/test/java/io/appium/java_client/android/AndroidTouchActionsTest.java
+++ src/test/java/io/appium/java_client/android/AndroidTouchActionsTest.java
@@ -19,15 +19,14 @@ package io.appium.java_client.android;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
-import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.interactions.touch.TouchActions;
 
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.TouchAction;
+
+import java.util.List;
 
 public class AndroidTouchActionsTest extends BaseAndroidTest {
 
