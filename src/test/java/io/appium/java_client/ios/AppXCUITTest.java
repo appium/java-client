90c2ba8482994ce6a5472551720f4f786efdceed
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2017-02-26 00:44:06 +0530

    Refractor

--- src/test/java/io/appium/java_client/ios/AppXCUITTest.java
+++ src/test/java/io/appium/java_client/ios/AppXCUITTest.java
@@ -17,15 +17,6 @@ public class AppXCUITTest extends BaseIOSTest {
      * initialization.
      */
     @BeforeClass public static void beforeClass() throws Exception {
-        service = AppiumDriverLocalService.buildDefaultService();
-        service.start();
-
-        if (service == null || !service.isRunning()) {
-            throw new AppiumServerHasNotBeenStartedLocallyException("An appium server node is not started!");
-        }
-
-        File appDir = new File("src/test/java/io/appium/java_client");
-        File app = new File(appDir, "TestApp.app.zip");
         DesiredCapabilities caps = DesiredCapabilities.iphone();
         caps.setCapability("appiumVersion", "1.6.3");
         caps.setCapability("deviceName","iPhone 7 Simulator");
