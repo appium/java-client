56260a070e8aa7dcaf6d95fc17cf1773d0a808c6
Jonathan Lipps <jlipps@gmail.com>
2017-06-16 16:22:14 -0700

    add tests for nativeWebTap method

---
+++ src/test/java/io/appium/java_client/ios/BaseSafariTest.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.appium.java_client.ios;
+
+import io.appium.java_client.remote.AutomationName;
+import io.appium.java_client.remote.IOSMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServerHasNotBeenStartedLocallyException;
+import org.junit.BeforeClass;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class BaseSafariTest extends BaseIOSTest {
+
+  @BeforeClass public static void beforeClass() throws Exception {
+    service = AppiumDriverLocalService.buildDefaultService();
+    service.start();
+
+    if (service == null || !service.isRunning()) {
+      throw new AppiumServerHasNotBeenStartedLocallyException("An appium server node is not started!");
+    }
+
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
+    capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
+    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.2");
+    //sometimes environment has performance problems
+    capabilities.setCapability(IOSMobileCapabilityType.LAUNCH_TIMEOUT, 500000);
+    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Simulator");
+    driver = new IOSDriver<>(service.getUrl(), capabilities);
+  }
+}
---
+++ src/test/java/io/appium/java_client/ios/IOSNativeWebTapSettingTest.java
@@ -0,0 +1,27 @@
+package io.appium.java_client.ios;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+public class IOSNativeWebTapSettingTest extends BaseSafariTest {
+
+  @Test public void nativeWebTapSettingTest() throws InterruptedException {
+    driver.get("https://saucelabs.com/test/guinea-pig");
+
+    // do a click with nativeWebTap turned on, and assert we get to the right page
+    driver.nativeWebTap(true);
+    WebElement el = driver.findElementById("i am a link");
+    el.click();
+    assertEquals(true, driver.getTitle().contains("I am another page title"));
+    driver.navigate().back();
+
+    // now do a click with it turned off and assert the same behavior
+    assertEquals(true, driver.getTitle().contains("I am a page title"));
+    driver.nativeWebTap(false);
+    el = driver.findElementById("i am a link");
+    el.click();
+    assertEquals(true, driver.getTitle().contains("I am another page title"));
+  }
+}
