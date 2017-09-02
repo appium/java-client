168d9d8cc068fb6417a1481237412efd02408574
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2017-09-02 12:17:30 +0300

    #714 FIX - test improvement

--- src/test/java/io/appium/java_client/android/OpenNotificationsTest.java
+++ src/test/java/io/appium/java_client/android/OpenNotificationsTest.java
@@ -1,12 +1,11 @@
 package io.appium.java_client.android;
 
 import static org.junit.Assert.assertNotEquals;
+import static org.openqa.selenium.By.id;
 
-import com.google.common.base.Function;
 
 import org.junit.Test;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
@@ -17,18 +16,15 @@ public class OpenNotificationsTest extends BaseAndroidTest {
         driver.closeApp();
         driver.openNotifications();
         WebDriverWait wait = new WebDriverWait(driver, 20);
-        assertNotEquals(0, wait.until(new Function<WebDriver, List<AndroidElement>>() {
-            @Override
-            public List<AndroidElement> apply(WebDriver input) {
-                List<AndroidElement> result = driver
-                        .findElementsById("com.android.systemui:id/carrier_label");
+        assertNotEquals(0, wait.until(input -> {
+            List<AndroidElement> result = input
+                    .findElements(id("com.android.systemui:id/carrier_label"));
 
-                if (result.size() == 0) {
-                    return null;
-                }
-
-                return result;
+            if (result.size() == 0) {
+                return null;
             }
+
+            return result;
         }).size());
     }
 }
