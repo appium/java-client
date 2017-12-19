da8d7f80734942ca47c0471862209013aa3a0fd4
Mykola Mokhnach <nick+1@wire.com>
2017-12-19 20:56:06 +0100

    Remove isPositive verification for point coordinates

--- src/main/java/io/appium/java_client/touch/offset/PointOption.java
+++ src/main/java/io/appium/java_client/touch/offset/PointOption.java
@@ -1,7 +1,5 @@
 package io.appium.java_client.touch.offset;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static java.lang.String.format;
 import static java.util.Optional.ofNullable;
 
 import io.appium.java_client.touch.ActionOptions;
@@ -12,8 +10,6 @@ import java.util.Map;
 public class PointOption<T extends PointOption<T>> extends ActionOptions<T> {
 
     protected Point coordinates;
-
-    private static final String ERROR_MESSAGE_TEMPLATE = "%s coordinate value should be equal or greater than zero";
 
     /**
      * It creates a built instance of {@link PointOption} which takes x and y coordinates.
@@ -36,14 +32,14 @@ public class PointOption<T extends PointOption<T>> extends ActionOptions<T> {
      * @return self-reference
      */
     public T withCoordinates(int xOffset, int yOffset) {
-        checkArgument(xOffset >= 0, format(ERROR_MESSAGE_TEMPLATE, "X"));
-        checkArgument(yOffset >= 0, format(ERROR_MESSAGE_TEMPLATE, "Y"));
         coordinates = new Point(xOffset, yOffset);
+        //noinspection unchecked
         return (T) this;
     }
 
     @Override
     protected void verify() {
+        //noinspection ResultOfMethodCallIgnored
         ofNullable(coordinates).orElseThrow(() -> new IllegalArgumentException(
                 "Coordinate values must be defined"));
     }
