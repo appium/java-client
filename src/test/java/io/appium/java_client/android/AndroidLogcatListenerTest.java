cbf4c43ccc683842dfc32414a8ce5cc16ff2fd36
Mykola Mokhnach <nick+1@wire.com>
2018-04-06 15:48:00 +0200

    Add missing dependencies

--- src/test/java/io/appium/java_client/android/AndroidLogcatListenerTest.java
+++ src/test/java/io/appium/java_client/android/AndroidLogcatListenerTest.java
@@ -14,44 +14,37 @@ public class AndroidLogcatListenerTest extends BaseAndroidTest {
     @Test
     public void verifyLogcatListenerCanBeAssigned() {
         final Semaphore messageSemaphore = new Semaphore(1);
-        final Semaphore connectedSemaphore = new Semaphore(1);
         final Duration timeout = Duration.ofSeconds(5);
 
+        driver.addLogcatListener(new MessagesHandler<String>() {
+            @Override
+            public void onMessage(String message) {
+                messageSemaphore.release();
+            }
+
+            @Override
+            public void onConnected() {
+                System.out.println("Connected to web socket");
+            }
+
+            @Override
+            public void onDisconnected() {
+                System.out.println("Disconnected from web socket");
+            }
+
+            @Override
+            public void onError(Throwable cause) {
+                cause.printStackTrace();
+            }
+        });
         try {
             driver.startLogcatBroadcast();
-            driver.addLogcatListener(new MessagesHandler<String>() {
-                @Override
-                public void onMessage(String message) {
-                    messageSemaphore.release();
-                }
-
-                @Override
-                public void onConnected() {
-                    connectedSemaphore.release();
-                }
-
-                @Override
-                public void onDisconnected() {
-                    // ignore
-                }
-
-                @Override
-                public void onError(Throwable cause) {
-                    // ignore
-                }
-            });
-
-            connectedSemaphore.acquire();
             messageSemaphore.acquire();
-
-            assertTrue(String.format("Didn't connect to the web socket after %s timeout", timeout),
-                    connectedSemaphore.tryAcquire(timeout.toMillis(), TimeUnit.MILLISECONDS));
             assertTrue(String.format("Didn't receive any log message after %s timeout", timeout),
                     messageSemaphore.tryAcquire(timeout.toMillis(), TimeUnit.MILLISECONDS));
         } catch (InterruptedException e) {
             throw new IllegalStateException(e);
         } finally {
-            connectedSemaphore.release();
             messageSemaphore.release();
             driver.stopLogcatBroadcast();
         }
