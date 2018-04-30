3bccbbaec7758de72f38d10339837b8d9ef0b79c
Mykola Mokhnach <nick+1@wire.com>
2018-04-30 19:39:01 +0200

    Support syslog broadcast with iOS (#871)
    * Support syslog broadcast with iOS
    
    * fix static import order

--- src/main/java/io/appium/java_client/ios/IOSDriver.java
+++ src/main/java/io/appium/java_client/ios/IOSDriver.java
@@ -56,7 +56,7 @@ public class IOSDriver<T extends WebElement>
     extends AppiumDriver<T>
     implements HidesKeyboardWithKeyName, ShakesDevice, HasIOSSettings,
         FindsByIosUIAutomation<T>, LocksDevice, PerformsTouchID, FindsByIosNSPredicate<T>,
-        FindsByIosClassChain<T>, PushesFiles, CanRecordScreen, HasIOSClipboard {
+        FindsByIosClassChain<T>, PushesFiles, CanRecordScreen, HasIOSClipboard, ListensToSyslogMessages {
 
     private static final String IOS_PLATFORM = MobilePlatform.IOS;
 
