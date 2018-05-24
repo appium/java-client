09a3c66c5bc0ba65016d6d6f7ff5f6b7ec23755d
Shridhar Kalagi <shridharkalagi@gmail.com>
2018-05-24 17:24:12 +0530

    Adding ESPRESSO automation name

--- src/main/java/io/appium/java_client/remote/AutomationName.java
+++ src/main/java/io/appium/java_client/remote/AutomationName.java
@@ -23,4 +23,5 @@ public interface AutomationName {
     String IOS_XCUI_TEST = "XCuiTest";
     String ANDROID_UIAUTOMATOR2 = "UIAutomator2";
     String YOUI_ENGINE = "youiengine";
+    String ESPRESSO = "Espresso";
 }
