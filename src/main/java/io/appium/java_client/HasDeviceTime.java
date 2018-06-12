5a2539a262f47808aeeb3b25538ea7fcce818cab
Mykola Mokhnach <nick+1@wire.com>
2018-06-12 10:58:10 +0200

    Update device time getters

--- src/main/java/io/appium/java_client/HasDeviceTime.java
+++ src/main/java/io/appium/java_client/HasDeviceTime.java
@@ -18,11 +18,44 @@ package io.appium.java_client;
 
 import static io.appium.java_client.MobileCommand.GET_DEVICE_TIME;
 
+import com.google.common.collect.ImmutableMap;
+
 import org.openqa.selenium.remote.Response;
 
 public interface HasDeviceTime extends ExecutesMethod {
-    /*
-        Gets device date and time for both iOS(Supports only real device) and Android devices
+
+    /**
+     * Gets device date and time for both iOS(host time is returned for simulators) and Android devices.
+     *
+     * @param format datetime format specifier:
+     *                          %% literal %             %n newline              %t tab
+     *                          %S seconds (00-60)       %M minute (00-59)       %m month (01-12)
+     *                          %H hour (0-23)           %I hour (01-12)         %p AM/PM
+     *                          %y short year (00-99)    %Y year                 %C century
+     *                          %a short weekday name    %A weekday name         %u day of week (1-7, 1=mon)
+     *                          %b short month name      %B month name           %Z timezone name
+     *                          %j day of year (001-366) %d day of month (01-31) %e day of month ( 1-31)
+     *                          %s seconds past the Epoch
+     *
+     *                          %U Week of year (0-53 start sunday)   %W Week of year (0-53 start monday)
+     *                          %V Week of year (1-53 start monday, week < 4 days not part of this year)
+     *
+     *                          %D = "%m/%d/%y"    %r = "%I : %M : %S %p"   %T = "%H:%M:%S"   %h = "%b"
+     *                          %x locale date     %X locale time           %c locale date/time
+     *               The specifier is only supported since Appium 1.8.2.
+     *               The default format specifier is "+%Y-%m-%dT%T%z".
+     * @return Device time string
+     */
+    default String getDeviceTime(String format) {
+        Response response = execute(GET_DEVICE_TIME, ImmutableMap.of("format", format));
+        return response.getValue().toString();
+    }
+
+    /**
+     * Gets device date and time for both iOS(host time is returned for simulators) and Android devices.
+     * The default time format for is ISO-8601 since Appium 1.8.2,
+     *
+     * @return Device time string
      */
     default String getDeviceTime() {
         Response response = execute(GET_DEVICE_TIME);
