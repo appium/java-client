52916b76c096e4db795ed41383be7dad8c7bad2a
Mykola Mokhnach <mokhnach@gmail.com>
2019-03-11 20:54:45 +0100

    Add the missing Android and iOS settings

--- src/main/java/io/appium/java_client/Setting.java
+++ src/main/java/io/appium/java_client/Setting.java
@@ -18,25 +18,37 @@ package io.appium.java_client;
 
 /**
  * Enums defining constants for Appium Settings which can be set and toggled during a test session.
+ * http://appium.io/docs/en/advanced-concepts/settings/
  */
 public enum Setting {
 
+    // Android
     IGNORE_UNIMPORTANT_VIEWS("ignoreUnimportantViews"),
     WAIT_FOR_IDLE_TIMEOUT("waitForIdleTimeout"),
     WAIT_FOR_SELECTOR_TIMEOUT("waitForSelectorTimeout"),
     WAIT_SCROLL_ACKNOWLEDGMENT_TIMEOUT("scrollAcknowledgmentTimeout"),
     WAIT_ACTION_ACKNOWLEDGMENT_TIMEOUT("actionAcknowledgmentTimeout"),
+    ALLOW_INVISIBLE_ELEMENTS("allowInvisibleElements"),
+    ENABLE_NOTIFICATION_LISTENER("enableNotificationListener"),
+    NORMALIZE_TAG_NAMES("normalizeTagNames"),
     KEY_INJECTION_DELAY("keyInjectionDelay"),
+    // iOS
+    MJPEG_SERVER_SCREENSHOT_QUALITY("mjpegServerScreenshotQuality"),
+    MJPEG_SERVER_FRAMERATE("mjpegServerFramerate"),
+    SCREENSHOT_QUALITY("screenshotQuality"),
     NATIVE_WEB_TAP("nativeWebTap"),
+    // Android and iOS
+    SHOULD_USE_COMPACT_RESPONSES("shouldUseCompactResponses"),
+    ELEMENT_RESPONSE_ATTRIBUTES("elementResponseAttributes"),
+    // All platforms
+    IMAGE_ELEMENT_TAP_STRATEGY("imageElementTapStrategy"),
     IMAGE_MATCH_THRESHOLD("imageMatchThreshold"),
     FIX_IMAGE_FIND_SCREENSHOT_DIMENSIONS("fixImageFindScreenshotDims"),
     FIX_IMAGE_TEMPLATE_SIZE("fixImageTemplateSize"),
     CHECK_IMAGE_ELEMENT_STALENESS("checkForImageElementStaleness"),
-    UPDATE_IMAGE_ELEMENT_POSITION("autoUpdateImageElementPosition"),
-    NORMALIZE_TAG_NAMES("normalizeTagNames"),
-    IMAGE_ELEMENT_TAP_STRATEGY("imageElementTapStrategy");
+    UPDATE_IMAGE_ELEMENT_POSITION("autoUpdateImageElementPosition");
 
-    private String name;
+    private final String name;
 
     Setting(String name) {
         this.name = name;
