cb498a3cdf62aa939dfa2731afaefe72cdaa4b09
Valery Yatsynovich <valfirst@yandex.ru>
2019-07-08 10:59:22 +0300

    Checkstyle updates (#1170)

--- src/main/java/io/appium/java_client/pagefactory/AndroidFindByChainSet.java
+++ src/main/java/io/appium/java_client/pagefactory/AndroidFindByChainSet.java
@@ -15,8 +15,10 @@ import java.lang.annotation.Target;
 @Retention(value = RUNTIME)
 public @interface AndroidFindByChainSet {
     /**
+     * An array of which builds a sequence of the chained searching for elements or a set of possible locators.
+     *
      * @return an array of {@link io.appium.java_client.pagefactory.AndroidFindBys} which builds a sequence of
-     * the chained searching for elements or a set of possible locators
+     *     the chained searching for elements or a set of possible locators
      */
     AndroidFindBys[] value();
 }
