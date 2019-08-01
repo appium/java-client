7d7e254d3a113ceaeca2939690859ba6c73b4185
Sai Krishna <saikrishna321@yahoo.com>
2019-08-01 08:54:51 +0100

    Mark Selendroid AutomationName as deprecated (#1198)

--- src/test/java/io/appium/java_client/pagefactory_tests/widget/tests/android/AndroidApp.java
+++ src/test/java/io/appium/java_client/pagefactory_tests/widget/tests/android/AndroidApp.java
@@ -8,10 +8,8 @@ import java.util.List;
 public class AndroidApp implements ExtendedApp {
 
     public static String ANDROID_DEFAULT_WIDGET_LOCATOR = "SOME_ANDROID_DEFAULT_LOCATOR";
-    public static String ANDROID_SELENDROID_WIDGET_LOCATOR = "SOME_SELENDROID_DEFAULT_LOCATOR";
 
     public static String ANDROID_EXTERNALLY_DEFINED_WIDGET_LOCATOR = "SOME_ANDROID_EXTERNALLY_DEFINED_LOCATOR";
-    public static String SELENDROID_EXTERNALLY_DEFINED_WIDGET_LOCATOR = "SOME_SELENDROID_EXTERNALLY_DEFINED_LOCATOR";
 
     @AndroidFindBy(uiAutomator = "SOME_ANDROID_DEFAULT_LOCATOR")
     private DefaultAndroidWidget singleAndroidWidget;
@@ -20,35 +18,35 @@ public class AndroidApp implements ExtendedApp {
     private List<DefaultAndroidWidget> multipleAndroidWidgets;
 
     /**
-     * This class is annotated by {@link AndroidFindBy} and @{@link SelendroidFindBy}.
+     * This class is annotated by {@link AndroidFindBy}
      * This field was added to check that locator is created correctly according to current platform
      * and current automation.
      */
     private AnnotatedAndroidWidget singleAnnotatedAndroidWidget;
 
     /**
-     * This class is annotated by {@link AndroidFindBy} and @{@link SelendroidFindBy}.
+     * This class is annotated by {@link AndroidFindBy}
      * This field was added to check that locator is created correctly according to current platform
      * and current automation.
      */
     private List<AnnotatedAndroidWidget> multipleAnnotatedAndroidWidgets;
 
     /**
-     * This class is not annotated by {@link AndroidFindBy} and {@link SelendroidFindBy}.
+     * This class is not annotated by {@link AndroidFindBy}
      * But the superclass is annotated by these annotations. This field was added to check that locator is
      * created correctly according to current platform and current automation.
      */
     private ExtendedAndroidWidget singleExtendedAndroidWidget;
 
     /**
-     * This class is not annotated by {@link AndroidFindBy} and {@link SelendroidFindBy}.
+     * This class is not annotated by {@link AndroidFindBy}
      * But the superclass is annotated by these annotations. This field was added to check that locator is
      * created correctly according to current platform and current automation.
      */
     private List<ExtendedAndroidWidget> multipleExtendedAndroidWidgets;
 
     /**
-     * The superclass is annotated by {@link AndroidFindBy} and {@link SelendroidFindBy}.
+     * The superclass is annotated by {@link AndroidFindBy}
      * However there is the field which is annotated by this annotations.
      * This field was added to check that locator is
      * created correctly according to current platform and current automation and
@@ -58,7 +56,7 @@ public class AndroidApp implements ExtendedApp {
     private ExtendedAndroidWidget singleOverriddenAndroidWidget;
 
     /**
-     * The superclass is annotated by {@link AndroidFindBy} and {@link SelendroidFindBy}.
+     * The superclass is annotated by {@link AndroidFindBy}
      * However there is the field which is annotated by this annotations.
      * This field was added to check that locator is
      * created correctly according to current platform and current automation and
