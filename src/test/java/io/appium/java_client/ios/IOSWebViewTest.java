7239a9d44cd088b6c457c93ac8a90a4cf797dd17
Wasiq Bhamla <wasbhamla2005@gmail.com>
2019-08-21 01:51:26 +0530

    Upgraded Selenium version to alpha 2. (#1210)
    * Upgraded Selenium version to alpha 2.
    
    * Updated for changes for Selenium alpha 2.
    
    * Updated for compilation errors.

--- src/test/java/io/appium/java_client/ios/IOSWebViewTest.java
+++ src/test/java/io/appium/java_client/ios/IOSWebViewTest.java
@@ -1,5 +1,6 @@
 package io.appium.java_client.ios;
 
+import static java.time.Duration.ofSeconds;
 import static org.junit.Assert.assertTrue;
 
 import io.appium.java_client.MobileBy;
@@ -9,14 +10,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 public class IOSWebViewTest extends BaseIOSWebViewTest {
 
     @Test public void webViewPageTestCase() throws InterruptedException {
-        new WebDriverWait(driver, 30)
+        new WebDriverWait(driver, ofSeconds(30))
                 .until(ExpectedConditions.presenceOfElementLocated(By.id("login")))
                 .click();
         driver.findElementByAccessibilityId("webView").click();
-        new WebDriverWait(driver, 30)
+        new WebDriverWait(driver, ofSeconds(30))
                 .until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Webview")));
         findAndSwitchToWebView();
         WebElement el = driver.findElementByPartialLinkText("login");
