16da49e6971d29bba7ceb49865808a89ecbbdd5e
Mykola Mokhnach <mokhnach@gmail.com>
2021-10-27 12:03:04 +0200

    Add docstring

--- src/main/java/io/appium/java_client/android/CanReplaceElementValue.java
+++ src/main/java/io/appium/java_client/android/CanReplaceElementValue.java
@@ -6,6 +6,12 @@ import io.appium.java_client.MobileCommand;
 import org.openqa.selenium.remote.RemoteWebElement;
 
 public interface CanReplaceElementValue extends ExecutesMethod {
+    /**
+     * Replaces element value with the given one.
+     *
+     * @param element The destination element.
+     * @param value The value to set.
+     */
     default void replaceElementValue(RemoteWebElement element, String value) {
         this.execute(MobileCommand.REPLACE_VALUE, ImmutableMap.of(
                 "id", element.getId(),
