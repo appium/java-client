c8d451e14db89f2fa19c5cfa6a38420d5fde71d1
Mykola Mokhnach <mokhnach@gmail.com>
2021-11-01 04:36:37 +0100

    chore: Remove unused entities (#1572)
    * chore: Remove unused entities
    
    * Tune deprecation notices
    
    * Add summary

--- src/main/java/io/appium/java_client/android/AndroidTouchAction.java
+++ src/main/java/io/appium/java_client/android/AndroidTouchAction.java
@@ -21,14 +21,16 @@ import io.appium.java_client.TouchAction;
 
 
 /**
- * Touch actions are deprecated.
- * Please use W3C Actions instead or the corresponding
- * extension methods for the driver (if available).
- * Check
- * - https://www.youtube.com/watch?v=oAJ7jwMNFVU
- * - https://appiumpro.com/editions/30-ios-specific-touch-action-methods
- * - https://appiumpro.com/editions/29-automating-complex-gestures-with-the-w3c-actions-api
- * for more details.
+ * Android-specific touch action.
+ *
+ * @deprecated Touch actions are deprecated.
+ *     Please use W3C Actions instead or the corresponding
+ *     extension methods for the driver (if available).
+ *     Check
+ *     - https://www.youtube.com/watch?v=oAJ7jwMNFVU
+ *     - https://appiumpro.com/editions/30-ios-specific-touch-action-methods
+ *     - https://appiumpro.com/editions/29-automating-complex-gestures-with-the-w3c-actions-api
+ *     for more details.
  */
 @Deprecated
 public class AndroidTouchAction extends TouchAction<AndroidTouchAction> {
