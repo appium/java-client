bd780ce63472ff07231cceea15495c9b2fee3164
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-08-05 09:36:32 -0700

    use ofNullable and orElse/Get

--- src/main/java/io/appium/java_client/remote/AppiumCommandExecutor.java
+++ src/main/java/io/appium/java_client/remote/AppiumCommandExecutor.java
@@ -65,16 +65,14 @@ public class AppiumCommandExecutor extends HttpCommandExecutor {
                                   HttpClient.Factory httpClientFactory,
                                   ClientConfig clientConfig) {
         super(additionalCommands,
-                clientConfig != null
-                        ? clientConfig
-                        : ClientConfig.defaultConfig()
-                            .baseUrl(Require.nonNull("Server URL", ofNullable(service)
-                                    .map(DriverService::getUrl)
-                                    .orElse(addressOfRemoteServer)))
-                            .readTimeout(DEFAULT_READ_TIMEOUT),
-                httpClientFactory != null
-                        ? httpClientFactory
-                        : getDefaultClientFactory()
+                ofNullable(clientConfig).orElse(
+                        ClientConfig.defaultConfig()
+                                .baseUrl(Require.nonNull("Server URL", ofNullable(service)
+                                        .map(DriverService::getUrl)
+                                        .orElse(addressOfRemoteServer)))
+                                .readTimeout(DEFAULT_READ_TIMEOUT)
+                ),
+                ofNullable(httpClientFactory).orElseGet(HttpCommandExecutor::getDefaultClientFactory)
         );
         serviceOptional = ofNullable(service);
     }
