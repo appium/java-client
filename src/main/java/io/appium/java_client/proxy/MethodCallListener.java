af348812dac9515fd14e9f92e5696cfc33f17007
Mykola Mokhnach <mokhnach@gmail.com>
2022-11-02 19:54:55 +0100

    Fix style

--- src/main/java/io/appium/java_client/proxy/MethodCallListener.java
+++ src/main/java/io/appium/java_client/proxy/MethodCallListener.java
@@ -21,64 +21,64 @@ import java.util.concurrent.Callable;
 
 public interface MethodCallListener {
 
-  /**
-   * The callback to be invoked before any public method of the proxy is called.
-   * The implementation is not expected to throw any exceptions. If a
-   * runtime exception is thrown then it is going to be silently logged.
-   *
-   * @param obj The proxy instance
-   * @param method Method to be called
-   * @param args Array of method arguments
-   */
-  default void beforeCall(Object obj, Method method, Object[] args) {
-    throw new NotImplementedException();
-  }
+    /**
+     * The callback to be invoked before any public method of the proxy is called.
+     * The implementation is not expected to throw any exceptions. If a
+     * runtime exception is thrown then it is going to be silently logged.
+     *
+     * @param obj    The proxy instance
+     * @param method Method to be called
+     * @param args   Array of method arguments
+     */
+    default void beforeCall(Object obj, Method method, Object[] args) {
+        throw new NotImplementedException();
+    }
 
-  /**
-   * Override this callback in order to change/customize the behavior
-   * of a single or multiple methods. The original method result
-   * will be replaced with the result returned by this callback.
-   * Also, any exception thrown by it will replace original method(s)
-   * exception.
-   *
-   * @param obj The proxy instance
-   * @param method Method to be replaced
-   * @param args Array of method arguments
-   * @param original The reference to the original method in case it is necessary to
-   *                 instrument its result.
-   * @return It is expected that the type of the returned argument could be cast to
-   * the returned type of the original method.
-   */
-  default Object call(Object obj, Method method, Object[] args, Callable<?> original) throws Throwable {
-    throw new NotImplementedException();
-  }
+    /**
+     * Override this callback in order to change/customize the behavior
+     * of a single or multiple methods. The original method result
+     * will be replaced with the result returned by this callback.
+     * Also, any exception thrown by it will replace original method(s)
+     * exception.
+     *
+     * @param obj      The proxy instance
+     * @param method   Method to be replaced
+     * @param args     Array of method arguments
+     * @param original The reference to the original method in case it is necessary to
+     *                 instrument its result.
+     * @return It is expected that the type of the returned argument could be cast to
+     * the returned type of the original method.
+     */
+    default Object call(Object obj, Method method, Object[] args, Callable<?> original) throws Throwable {
+        throw new NotImplementedException();
+    }
 
-  /**
-   * The callback to be invoked after any public method of the proxy is called.
-   * The implementation is not expected to throw any exceptions. If a
-   * runtime exception is thrown then it is going to be silently logged.
-   *
-   * @param obj The proxy instance
-   * @param method Method to be called
-   * @param args Array of method arguments
-   */
-  default void afterCall(Object obj, Method method, Object[] args, Object result) {
-    throw new NotImplementedException();
-  }
+    /**
+     * The callback to be invoked after any public method of the proxy is called.
+     * The implementation is not expected to throw any exceptions. If a
+     * runtime exception is thrown then it is going to be silently logged.
+     *
+     * @param obj    The proxy instance
+     * @param method Method to be called
+     * @param args   Array of method arguments
+     */
+    default void afterCall(Object obj, Method method, Object[] args, Object result) {
+        throw new NotImplementedException();
+    }
 
-  /**
-   * The callback to be invoked if a public method or its
-   * {@link #call(Object, Method, Object[], Callable) Call} replacement throws an exception.
-   *
-   * @param obj The proxy instance
-   * @param method Method to be called
-   * @param args Array of method arguments
-   * @param e Exception instance thrown by the original method invocation.
-   * @return You could either (re)throw the exception in this callback or
-   * overwrite the behavior and return a result from it. It is expected that the
-   * type of the returned argument could be cast to the returned type of the original method.
-   */
-  default Object onError(Object obj, Method method, Object[] args, Throwable e) throws Throwable {
-    throw new NotImplementedException();
-  }
+    /**
+     * The callback to be invoked if a public method or its
+     * {@link #call(Object, Method, Object[], Callable) Call} replacement throws an exception.
+     *
+     * @param obj    The proxy instance
+     * @param method Method to be called
+     * @param args   Array of method arguments
+     * @param e      Exception instance thrown by the original method invocation.
+     * @return You could either (re)throw the exception in this callback or
+     * overwrite the behavior and return a result from it. It is expected that the
+     * type of the returned argument could be cast to the returned type of the original method.
+     */
+    default Object onError(Object obj, Method method, Object[] args, Throwable e) throws Throwable {
+        throw new NotImplementedException();
+    }
 }
