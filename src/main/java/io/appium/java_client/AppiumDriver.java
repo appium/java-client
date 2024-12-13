a054753f846e5fa05042134a38bf3ba1077f0fa4
Mykola Mokhnach <mokhnach@gmail.com>
2024-12-13 22:27:49 +0100

    feat: Implement HasBiDi interface support in AppiumDriver

--- src/main/java/io/appium/java_client/AppiumDriver.java
+++ src/main/java/io/appium/java_client/AppiumDriver.java
@@ -30,6 +30,8 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.SessionNotCreatedException;
 import org.openqa.selenium.UnsupportedCommandException;
 import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.bidi.BiDi;
+import org.openqa.selenium.bidi.HasBiDi;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DriverCommand;
 import org.openqa.selenium.remote.ErrorHandler;
@@ -42,6 +44,8 @@ import org.openqa.selenium.remote.html5.RemoteLocationContext;
 import org.openqa.selenium.remote.http.HttpClient;
 import org.openqa.selenium.remote.http.HttpMethod;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Collections;
@@ -66,7 +70,8 @@ public class AppiumDriver extends RemoteWebDriver implements
         LogsEvents,
         HasBrowserCheck,
         CanRememberExtensionPresence,
-        HasSettings {
+        HasSettings,
+        HasBiDi {
 
     private static final ErrorHandler ERROR_HANDLER = new ErrorHandler(new ErrorCodesMobile(), true);
     // frequently used command parameters
@@ -76,6 +81,8 @@ public class AppiumDriver extends RemoteWebDriver implements
     protected final RemoteLocationContext locationContext;
     private final ExecuteMethod executeMethod;
     private final Set<String> absentExtensionNames = new HashSet<>();
+    protected URI biDiUri;
+    protected BiDi biDi;
 
     /**
      * Creates a new instance based on command {@code executor} and {@code capabilities}.
@@ -171,6 +178,134 @@ public class AppiumDriver extends RemoteWebDriver implements
         setSessionId(sessionAddress.getId());
     }
 
+    @Override
+    public ExecuteMethod getExecuteMethod() {
+        return executeMethod;
+    }
+
+    /**
+     * This method is used to get build version status of running Appium server.
+     *
+     * @return map containing version details
+     */
+    public Map<String, Object> getStatus() {
+        //noinspection unchecked
+        return (Map<String, Object>) execute(DriverCommand.STATUS).getValue();
+    }
+
+    /**
+     * This method is used to add custom appium commands in Appium 2.0.
+     *
+     * @param httpMethod the available {@link HttpMethod}.
+     * @param url        The url to URL template as https://www.w3.org/TR/webdriver/#endpoints.
+     * @param methodName The name of custom appium command.
+     */
+    public void addCommand(HttpMethod httpMethod, String url, String methodName) {
+        switch (httpMethod) {
+            case GET:
+                MobileCommand.commandRepository.put(methodName, MobileCommand.getC(url));
+                break;
+            case POST:
+                MobileCommand.commandRepository.put(methodName, MobileCommand.postC(url));
+                break;
+            case DELETE:
+                MobileCommand.commandRepository.put(methodName, MobileCommand.deleteC(url));
+                break;
+            default:
+                throw new WebDriverException(String.format("Unsupported HTTP Method: %s. Only %s methods are supported",
+                        httpMethod,
+                        Arrays.toString(HttpMethod.values())));
+        }
+        ((AppiumCommandExecutor) getCommandExecutor()).refreshAdditionalCommands();
+    }
+
+    @Override
+    public Response execute(String driverCommand, Map<String, ?> parameters) {
+        return super.execute(driverCommand, parameters);
+    }
+
+    @Override
+    public Response execute(String command) {
+        return super.execute(command, Collections.emptyMap());
+    }
+
+    @Override
+    public <X> X getScreenshotAs(OutputType<X> outputType) {
+        // TODO: Eventually we should not override this method.
+        // TODO: Although, we have a legacy burden,
+        // TODO: so it's impossible to do it the other way as of Oct 29 2022.
+        // TODO: See https://github.com/SeleniumHQ/selenium/issues/11168
+        return super.getScreenshotAs(new OutputType<X>() {
+            @Override
+            public X convertFromBase64Png(String base64Png) {
+                String rfc4648Base64 = base64Png.replaceAll("\\r?\\n", "");
+                return outputType.convertFromBase64Png(rfc4648Base64);
+            }
+
+            @Override
+            public X convertFromPngBytes(byte[] png) {
+                return outputType.convertFromPngBytes(png);
+            }
+        });
+    }
+
+    @Override
+    public AppiumDriver assertExtensionExists(String extName) {
+        if (absentExtensionNames.contains(extName)) {
+            throw new UnsupportedCommandException();
+        }
+        return this;
+    }
+
+    @Override
+    public AppiumDriver markExtensionAbsence(String extName) {
+        absentExtensionNames.add(extName);
+        return this;
+    }
+
+    @Override
+    public Optional<BiDi> maybeGetBiDi() {
+        return Optional.ofNullable(this.biDi);
+    }
+
+    protected HttpClient getHttpClient() {
+        return ((HttpCommandExecutor) getCommandExecutor()).client;
+    }
+
+    @Override
+    protected void startSession(Capabilities capabilities) {
+        var response = Optional.ofNullable(
+                execute(DriverCommand.NEW_SESSION(singleton(capabilities)))
+        ).orElseThrow(() -> new SessionNotCreatedException(
+                "The underlying command executor returned a null response."
+        ));
+
+        var rawCapabilities = Optional.ofNullable(response.getValue())
+                .map(value -> {
+                    if (!(value instanceof Map)) {
+                        throw new SessionNotCreatedException(String.format(
+                                "The underlying command executor returned a response "
+                                        + "with a non well formed payload: %s", response)
+                        );
+                    }
+                    //noinspection unchecked
+                    return (Map<String, Object>) value;
+                })
+                .orElseThrow(() -> new SessionNotCreatedException(
+                        "The underlying command executor returned a response without payload: " + response)
+                );
+
+        // TODO: remove this workaround for Selenium API enforcing some legacy capability values in major version
+        rawCapabilities.remove("platform");
+        if (rawCapabilities.containsKey(CapabilityType.BROWSER_NAME)
+                && isNullOrEmpty((String) rawCapabilities.get(CapabilityType.BROWSER_NAME))) {
+            rawCapabilities.remove(CapabilityType.BROWSER_NAME);
+        }
+        this.capabilities = new BaseOptions<>(rawCapabilities);
+        this.initBiDi(capabilities);
+        setSessionId(response.getSessionId());
+    }
+
     /**
      * Changes platform name if it is not set and returns merged capabilities.
      *
@@ -219,125 +354,27 @@ public class AppiumDriver extends RemoteWebDriver implements
         return ensureAutomationName(capsWithPlatformFixed, defaultAutomationName);
     }
 
-    @Override
-    public ExecuteMethod getExecuteMethod() {
-        return executeMethod;
-    }
-
-    /**
-     * This method is used to get build version status of running Appium server.
-     *
-     * @return map containing version details
-     */
-    public Map<String, Object> getStatus() {
-        //noinspection unchecked
-        return (Map<String, Object>) execute(DriverCommand.STATUS).getValue();
-    }
-
-    /**
-     * This method is used to add custom appium commands in Appium 2.0.
-     *
-     * @param httpMethod the available {@link HttpMethod}.
-     * @param url        The url to URL template as https://www.w3.org/TR/webdriver/#endpoints.
-     * @param methodName The name of custom appium command.
-     */
-    public void addCommand(HttpMethod httpMethod, String url, String methodName) {
-        switch (httpMethod) {
-            case GET:
-                MobileCommand.commandRepository.put(methodName, MobileCommand.getC(url));
-                break;
-            case POST:
-                MobileCommand.commandRepository.put(methodName, MobileCommand.postC(url));
-                break;
-            case DELETE:
-                MobileCommand.commandRepository.put(methodName, MobileCommand.deleteC(url));
-                break;
-            default:
-                throw new WebDriverException(String.format("Unsupported HTTP Method: %s. Only %s methods are supported",
-                        httpMethod,
-                        Arrays.toString(HttpMethod.values())));
+    private void initBiDi(Capabilities responseCaps) {
+        var webSocketUrl = CapabilityHelpers.getCapability(responseCaps, "webSocketUrl", String.class);
+        if (webSocketUrl == null) {
+            return;
         }
-        ((AppiumCommandExecutor) getCommandExecutor()).refreshAdditionalCommands();
-    }
-
-    @Override
-    protected void startSession(Capabilities capabilities) {
-        var response = Optional.ofNullable(
-                execute(DriverCommand.NEW_SESSION(singleton(capabilities)))
-        ).orElseThrow(() -> new SessionNotCreatedException(
-                    "The underlying command executor returned a null response."
-        ));
-
-        var rawCapabilities = Optional.ofNullable(response.getValue())
-                .map(value -> {
-                    if (!(value instanceof Map)) {
-                        throw new SessionNotCreatedException(String.format(
-                                "The underlying command executor returned a response "
-                                        + "with a non well formed payload: %s", response)
-                        );
-                    }
-                    //noinspection unchecked
-                    return (Map<String, Object>) value;
-                })
-                .orElseThrow(() -> new SessionNotCreatedException(
-                    "The underlying command executor returned a response without payload: " + response)
-                );
-
-        // TODO: remove this workaround for Selenium API enforcing some legacy capability values in major version
-        rawCapabilities.remove("platform");
-        if (rawCapabilities.containsKey(CapabilityType.BROWSER_NAME)
-                && isNullOrEmpty((String) rawCapabilities.get(CapabilityType.BROWSER_NAME))) {
-            rawCapabilities.remove(CapabilityType.BROWSER_NAME);
+        try {
+            this.biDiUri = new URI(webSocketUrl);
+        } catch (URISyntaxException e) {
+            // no valid url -> no BiDi
+            return;
         }
-        this.capabilities = new BaseOptions<>(rawCapabilities);
-        setSessionId(response.getSessionId());
-    }
-
-    @Override
-    public Response execute(String driverCommand, Map<String, ?> parameters) {
-        return super.execute(driverCommand, parameters);
-    }
-
-    @Override
-    public Response execute(String command) {
-        return super.execute(command, Collections.emptyMap());
-    }
-
-    @Override
-    public <X> X getScreenshotAs(OutputType<X> outputType) {
-        // TODO: Eventually we should not override this method.
-        // TODO: Although, we have a legacy burden,
-        // TODO: so it's impossible to do it the other way as of Oct 29 2022.
-        // TODO: See https://github.com/SeleniumHQ/selenium/issues/11168
-        return super.getScreenshotAs(new OutputType<X>() {
-            @Override
-            public X convertFromBase64Png(String base64Png) {
-                String rfc4648Base64 = base64Png.replaceAll("\\r?\\n", "");
-                return outputType.convertFromBase64Png(rfc4648Base64);
-            }
-
-            @Override
-            public X convertFromPngBytes(byte[] png) {
-                return outputType.convertFromPngBytes(png);
-            }
-        });
-    }
-
-    @Override
-    public AppiumDriver assertExtensionExists(String extName) {
-        if (absentExtensionNames.contains(extName)) {
-            throw new UnsupportedCommandException();
+        var executor = getCommandExecutor();
+        final HttpClient wsClient;
+        if (executor instanceof AppiumCommandExecutor) {
+            var wsConfig = ((AppiumCommandExecutor) executor).getAppiumClientConfig().baseUri(biDiUri);
+            wsClient = ((AppiumCommandExecutor) executor).getHttpClientFactory().createClient(wsConfig);
+        } else {
+            var wsConfig = AppiumClientConfig.defaultConfig().baseUri(biDiUri);
+            wsClient = HttpClient.Factory.createDefault().createClient(wsConfig);
         }
-        return this;
-    }
-
-    @Override
-    public AppiumDriver markExtensionAbsence(String extName) {
-        absentExtensionNames.add(extName);
-        return this;
-    }
-
-    protected HttpClient getHttpClient() {
-        return ((HttpCommandExecutor) getCommandExecutor()).client;
+        var biDiConnection = new org.openqa.selenium.bidi.Connection(wsClient, biDiUri.toString());
+        this.biDi = new BiDi(biDiConnection);
     }
 }
