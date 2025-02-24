338f05383da497f67fc1d2a8a31184f8fd5bd364
Alexander <2487708+alexanderkaiser@users.noreply.github.com>
2025-02-24 14:25:17 +0100

    CheckStyle: re-format new code to comply with appium-style.xml

--- src/test/java/io/appium/java_client/remote/options/BaseOptionsTest.java
+++ src/test/java/io/appium/java_client/remote/options/BaseOptionsTest.java
@@ -1,25 +1,24 @@
 package io.appium.java_client.remote.options;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class BaseOptionsTest {
 
-
-  @ParameterizedTest
-  @CsvSource({
-      "test, appium:test",
-      "appium:test, appium:test",
-      "browserName, browserName",
-      "digital.ai:accessKey, digital.ai:accessKey",
-      "digital-ai:accessKey, digital-ai:accessKey",
-      "digital-ai:my_custom-cap:xyz, digital-ai:my_custom-cap:xyz",
-      "digital-ai:my_custom-cap?xyz, digital-ai:my_custom-cap?xyz",
-  })
-  void verifyW3CMapping(String capName, String expected) {
-    var w3cName = BaseOptions.toW3cName(capName);
-    assertEquals(expected, w3cName);
-  }
+    @ParameterizedTest
+    @CsvSource({
+        "test, appium:test",
+        "appium:test, appium:test",
+        "browserName, browserName",
+        "digital.ai:accessKey, digital.ai:accessKey",
+        "digital-ai:accessKey, digital-ai:accessKey",
+        "digital-ai:my_custom-cap:xyz, digital-ai:my_custom-cap:xyz",
+        "digital-ai:my_custom-cap?xyz, digital-ai:my_custom-cap?xyz",
+    })
+    void verifyW3CMapping(String capName, String expected) {
+        var w3cName = BaseOptions.toW3cName(capName);
+        assertEquals(expected, w3cName);
+    }
 }
