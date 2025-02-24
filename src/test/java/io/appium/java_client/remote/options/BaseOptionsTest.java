af62a4d26092e23e745eb5cc08b310c0a8d3fb48
Alexander <alexander.kaiser@gematik.de>
2025-02-24 11:29:52 +0100

    feat: Allow extension capability keys to contain special characters

---
+++ src/test/java/io/appium/java_client/remote/options/BaseOptionsTest.java
@@ -0,0 +1,26 @@
+package io.appium.java_client.remote.options;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class BaseOptionsTest {
+
+
+  @ParameterizedTest
+  @CsvSource({
+      "test, appium:test",
+      "appium:test, appium:test",
+      "browserName, browserName",
+      "digital.ai:accessKey, digital.ai:accessKey",
+      "digital-ai:accessKey, digital-ai:accessKey",
+      "<VENDOR-KEY>:<CAP-NAME>:<SUB-CAP-NAME>, <VENDOR-KEY>:<CAP-NAME>:<SUB-CAP-NAME>",
+      "digital-ai:my_custom-cap:xyz, digital-ai:my_custom-cap:xyz",
+      "digital-ai:my_custom-cap?xyz, digital-ai:my_custom-cap?xyz",
+  })
+  void verifyW3CMapping(String capName, String expected) {
+    var w3cName = BaseOptions.toW3cName(capName);
+    assertEquals(expected, w3cName);
+  }
+}
